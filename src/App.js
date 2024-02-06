8a89d64c00e8cad1aa60fbe8170453ac79efacde
davep80 <davidpaq1@gmail.com>
2024-02-06 02:05:32 -0500

    readme and react project

--- src/App.js
+++ src/App.js
@@ -1,22 +1,10 @@
-import logo from './logo.svg';
-import './App.css';
-
+import useNearMe from "./hooks/useNearMe";
 function App() {
+  const myLocation = useNearMe();
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+      <header className="App-header" style={{ textAlign: "center" }}>
+        {myLocation ? <h3>+ {myLocation}</h3> : <h3>Turn On Location</h3>}
       </header>
     </div>
   );
